82ca3c585aebfc24cc024e6affa78e0a881f59dc
rabby6530 <you@example.com>
2023-03-16 10:35:44 +0600

    fixed-quizData-loading

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ startQuiz.addEventListener("click", () => {
 
   let x = setInterval(() => {
     if (counterNum < 0) {
+      //  contDown variable name was wrong it fixed me .
       countDown.classList.remove("flex");
       countDown.classList.add("hidden");
       counterNum = 3;
@@ -46,25 +47,29 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
 };
 
 // Displaying quiz on quiz page
+// 72 line dynamic link was wrong it fixed me the right away.
 const displayQuiz = (data) => {
+  
   if (!data) {
     quizContainer.innerHTML = "";
     return;
   }
 
   data.forEach((quiz, i) => {
+   
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
@@ -74,6 +79,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
+// addEventListener declaration was wrong it fixed me.
 document.querySelector("#submit").addEventListener("click",  () => {
   if (answers.length < 6) {
     return;
